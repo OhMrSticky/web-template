065a6b87317701f2740247e58efff60a4f212f90
Emily Smith <emily.smith@staples.com>
2015-11-05 16:06:09 -0800

    just got scss working using multiple src files

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,21 +5,33 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
 
         concat: {
-            dist: {
+            js: {
 				src: [
 					'dist/js/vendor/*.js', // All JS in the vendor folder
 					'src/js/*.js'  // All custom JS
 				],
-				dest: 'dist/js/production.js',
+				dest: 'dist/js/main.js',
 			}
-        }
+        },
+		
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed'
+				},
+				files: {
+					'dist/css/main.css': 'src/scss/main.scss'
+				}
+			} 
+		}
 
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('default', ['concat', 'sass']);
 
 };
